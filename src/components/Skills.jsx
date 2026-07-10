5a7aa1c66a4328783538838136317bec85a4759b
Chriss George <chrissgeorgek@gmail.com>
2026-07-10 14:57:14 +0530

    Added Node.js and Express.js skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,8 @@ import {
     FaBootstrap,
     FaGitAlt,
     FaGithub,
-    FaAws
+    FaAws,
+    FaNodeJs
   } from "react-icons/fa";
   
   import {
@@ -14,26 +15,29 @@ import {
     SiPostgresql,
     SiMysql,
     SiPostman,
-    SiJavascript
+    SiJavascript,
+    SiExpress
   } from "react-icons/si";
   
   function Skills() {
   
     const skills = [
-      { icon: <FaPython />, name: "Python" },
-      { icon: <SiDjango />, name: "Django" },
-      { icon: <FaReact />, name: "React" },
-      { icon: <SiJavascript />, name: "JavaScript" },
-      { icon: <FaHtml5 />, name: "HTML5" },
-      { icon: <FaCss3Alt />, name: "CSS3" },
-      { icon: <FaBootstrap />, name: "Bootstrap" },
-      { icon: <SiPostgresql />, name: "PostgreSQL" },
-      { icon: <SiMysql />, name: "MySQL" },
-      { icon: <FaGitAlt />, name: "Git" },
-      { icon: <FaGithub />, name: "GitHub" },
-      { icon: <SiPostman />, name: "Postman" },
-      { icon: <FaAws />, name: "AWS Basics" }
-    ];
+        { icon: <FaPython />, name: "Python" },
+        { icon: <SiDjango />, name: "Django" },
+        { icon: <FaReact />, name: "React" },
+        { icon: <FaNodeJs />, name: "Node.js" },
+        { icon: <SiExpress />, name: "Express.js" },
+        { icon: <SiJavascript />, name: "JavaScript" },
+        { icon: <FaHtml5 />, name: "HTML5" },
+        { icon: <FaCss3Alt />, name: "CSS3" },
+        { icon: <FaBootstrap />, name: "Bootstrap" },
+        { icon: <SiPostgresql />, name: "PostgreSQL" },
+        { icon: <SiMysql />, name: "MySQL" },
+        { icon: <FaGitAlt />, name: "Git" },
+        { icon: <FaGithub />, name: "GitHub" },
+        { icon: <SiPostman />, name: "Postman" },
+        { icon: <FaAws />, name: "AWS Basics" }
+      ];
   
     return (
       <section className="section">
